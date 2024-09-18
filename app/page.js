7ceaccf67e0ffb0fecd7190a3f5f99ebca934ddc
farhan-mazumder <farhanmazumder75@gmail.com>
2024-09-18 14:41:56 +0600

    practice layout and page concept.

--- app/page.js
+++ app/page.js
@@ -6,6 +6,15 @@ export default function Home() {
       <h2 className="mt-20 text-4xl font-bold text-center text-red-500">
         Hello World
       </h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, iure?
+        Tenetur, in recusandae ab eveniet saepe est incidunt eius fugiat,
+        possimus at voluptates distinctio accusantium voluptatibus quos et autem
+        accusamus. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Nam, iure? Tenetur, in recusandae ab eveniet saepe est incidunt eius
+        fugiat, possimus at voluptates distinctio accusantium voluptatibus quos
+        et autem accusamus.
+      </p>
     </>
   );
 }
