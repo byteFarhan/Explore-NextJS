218999733cabc11049185a426ea9b11c85c354cd
farhan-mazumder <farhanmazumder75@gmail.com>
2024-09-19 02:09:24 +0600

    explore loading in nextJS.

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-semibold text-center">Loading...</h1>
+    </div>
+  );
+};
+
+export default loading;
