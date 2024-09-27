fe04cc976d5f9c4a88d156bc26219d9623af35ef
farhan-mazumder <farhanmazumder75@gmail.com>
2024-09-27 23:50:30 +0600

    Explore use case of metadata.

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+  title: "About Us",
+  description: "This is about us page!",
+};
+
 const AboutLayout = ({ children }) => {
   return (
     <div>
