58bffbfee9ca8ac75e9f1f0c789280d75851d8a5
farhan-mazumder <farhanmazumder75@gmail.com>
2024-10-01 18:27:32 +0600

    explore Group Routing in nextJS.

---
+++ app/(dashbord)/userDashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">This is user dashboard.</h1>
+    </div>
+  );
+};
+
+export default UserDashboard;
