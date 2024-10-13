45e8ed77d0ccda052fc93c27bdfa2a58e9e64e2b
farhan-mazumder <farhanmazumder75@gmail.com>
2024-10-13 10:45:31 +0600

    explore middleware in NextJS.

---
+++ middleware.js
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+
+// export const middleware = (request) => {
+//   //   console.log(request);
+//   // We can use condition instead of using cofig for middleware example mentioned below
+//   if (request.nextUrl.pathname.startsWith("/about")) {
+//     // console.log(true);
+//     return NextResponse.rewrite(new URL("/about/vision/mission", request.url));
+//   }
+//   //    return NextResponse.redirect(new URL("/about/vision/mission", request.url));
+//   //   return NextResponse.rewrite(new URL("/about/vision/mission", request.url));
+//   //   return NextResponse.next();
+// };
+
+// export const config = {
+//   //   matcher: "/about",
+//   matcher: ["/about", "/services"],
+// };
+
+// Another use case of condition in middleware can be checking is user available or not
+// const user = false;
+// export const middleware = (request) => {
+//   if (!user) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
+//   return NextResponse.next();
+// };
+
+// export const config = {
+//   matcher: "/adminDashboard",
+// };
+
+// using middleware with browser cookies
+export const middleware = (requset) => {
+  const cookie = "farhan-mazumder";
+  const browserCookies = requset.cookies.get("token");
+  //   console.log(browserCookies);
+  if (!browserCookies || browserCookies.value !== cookie) {
+    return NextResponse.redirect(new URL("/login", requset.url));
+  }
+  return NextResponse.next();
+};
+export const config = {
+  matcher: "/adminDashboard",
+};
